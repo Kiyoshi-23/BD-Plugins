d965cd265f616e5bc7e6389de3320dccda876cae
Sashwath <90008981+Kiyoshi-23@users.noreply.github.com>
2022-03-04 17:50:09 -0600

    add constants and info

---
+++ Hide-Menu-Icons/HideMenuIcons.plugin.js
@@ -0,0 +1,31 @@
+/**
+* @name HideMenuIcons
+* @author Kiyoshi
+* @version 1.0
+* @description Slides the menu icons in the far-left of your discord with the click of a mouse for a much more clear view.
+
+* @website https://github.com/Kiyoshi-23/BD-Plugins
+* @source https://github.com/Kiyoshi-23/BD-Plugins/blob/main/Hide-Menu-Icons/HideMenuIcons.plugin.js
+* @updateUrl https://raw.githubusercontent.com/Kiyoshi-23/BD-Plugins/main/Hide-Menu-Icons/HideMenuIcons.plugin.js
+*/
+
+const config = {
+  info: {
+    id: "HideMenuIcons",
+    name: "Hide-Menu-Icons",
+    description: "Slides the menu icons in the far-left of your discord with the click of a mouse for a much more clear view",
+    version: "1.0",
+    author: "Kiyoshi",
+    updateUrl: "https://raw.githubusercontent.com/Kiyoshi-23/BD-Plugins/main/Hide-Menu-Icons/HideMenuIcons.plugin.js"
+  },
+  constants: {
+    cssStyle: "HideMenuStyle",
+    parentID: "menusParent",
+    buttonID: "menusButton",
+    buttonHidden: "menusHidden",
+    buttonVisible: "menusVisible",
+    hideElementName: "hideMenuElement"
+    forceWidth: "forceIconWidth",
+    animationTime: 300
+  }
+}
