7dc39ddc89f3fa3896b34eeb3d7aacef4099bc5c
Sashwath <90008981+Kiyoshi-23@users.noreply.github.com>
2022-03-05 12:00:48 -0600

    add button css and inject css func

--- Hide-Menu-Icons/HideMenuIcons.plugin.js
+++ Hide-Menu-Icons/HideMenuIcons.plugin.js
@@ -35,3 +35,46 @@ class HideMenuIcons {
   getDescription() { return config.info.description; }
   getVersion() { return config.info.version; }
   getAuthor() { return config.info.author; }
+
+  // Now, we need to create a function to inject the CSS. This includes the button CSS inside a long string.  
+  // We could just use it as a long string but we can define it to use it multiple times. 
+  injectCSS = `#${config.constants.buttonID} {
+          width: 13px;
+          min-width: 13px;
+          height: 13px;
+          min-height: 13px;
+          display: flex;
+          cursor: pointer;
+          opacity: 1;
+      }
+
+      .theme-dark #${config.constants.buttonID}.${config.constants.buttonVisible} {
+        background: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB2aWV3Qm94PSIwIDAgMjQgMjQiIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCI+CgkJCgkJCgkJCgkJCgkJPHBhdGggZD0iTTguNTkgMTYuNTlMMTMuMTcgMTIgOC41OSA3LjQxIDEwIDZsNiA2LTYgNi0xLjQxLTEuNDF6IiBmaWxsPSIjYjliYmJlIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiLz4KCTwvc3ZnPg==) no-repeat;
+      }
+
+      .theme-dark #${config.constants.buttonID}.${config.constants.buttonHidden} {
+        background: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB2aWV3Qm94PSIwIDAgMjQgMjQiIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCI+PHBhdGggZD0iTTcuMTE1KzE1LjcwNUwxMS43MDUrMTEuMTI1TDE2LjI5NSsxNS43MDVMMTcuNzA1KzE0LjI5NUwxMS43MDUrOC4yOTVMNS43MDUrMTQuMjk1QzUuNzA1KzE0LjI5NSs3LjExNSsxNS43MDUrNy4xMTUrMTUuNzA1WiIgZmlsbD0iIzRmNTY2MCIgZmlsbC1ydWxlPSJldmVub2RkIi8+PC9zdmc+) no-repeat;
+      }
+
+      .theme-light #${config.constants.buttonID}.${config.constants.buttonVisible} {
+        background: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB2aWV3Qm94PSIwIDAgMjQgMjQiIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCI+CgkJCgkJCgkJCgkJCgkJPHBhdGggZD0iTTguNTkgMTYuNTlMMTMuMTcgMTIgOC41OSA3LjQxIDEwIDZsNiA2LTYgNi0xLjQxLTEuNDF6IiBmaWxsPSIjYjliYmJlIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiLz4KCTwvc3ZnPg==) no-repeat;
+        opacity: 0.75;
+      }
+
+      .theme-light #${config.constants.buttonID}.${config.constants.buttonHidden} {
+        background: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB2aWV3Qm94PSIwIDAgMjQgMjQiIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCI+PHBhdGggZD0iTTcuMTE1KzE1LjcwNUwxMS43MDUrMTEuMTI1TDE2LjI5NSsxNS43MDVMMTcuNzA1KzE0LjI5NUwxMS43MDUrOC4yOTVMNS43MDUrMTQuMjk1QzUuNzA1KzE0LjI5NSs3LjExNSsxNS43MDUrNy4xMTUrMTUuNzA1WiIgZmlsbD0iIzRmNTY2MCIgZmlsbC1ydWxlPSJldmVub2RkIi8+PC9zdmc+) no-repeat;
+        opacity: 0.75;
+
+      ${config.constants.forceWidth} {
+            width: 0px;
+      }
+      
+      #${config.constants.hideElementName} {
+        trasform: translateX(200px);
+        opacity: 0;
+      }
+
+      .${this.buttonClasses.buttons} {
+            transition: transform ${config.constants.animationTime}ms ease, opacity ${config.constants.animationTime}ms ease;
+      }
+      }`
